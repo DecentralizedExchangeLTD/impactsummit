984730b7857b34f7044fb352f601a675c0d237ac
chukusuccess <successchuku5@gmail.com>
2023-11-01 20:47:04 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,318 @@
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import "./globals.css";
 
 export default function Home() {
+  const [showNav, setShowNav] = React.useState();
+  const menuItems = [
+    { name: "Hackathon", id: "hackathon" },
+    { name: "Schedule", id: "schedule" },
+    { name: "Speakers", id: "speakers" },
+    { name: "Tickets", id: "tickets" },
+    { name: "Impact", id: "impact" },
+    { name: "Contact Us", id: "contact" },
+  ];
+
+  function scrollToSection(e, id) {
+    e.preventDefault();
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="sm:mt-28 mt-14">
+        <nav className="fixed hidden sm:block top-0 bg-white w-full drop-shadow-md">
+          <h1 className="absolute left-10 top-10 font-bold text-xl">
+            Impact Summit
+          </h1>
+          <ul className={` flex items-center justify-center h-28 gap-5`}>
+            {menuItems.map((item, index) => (
+              <li
+                className="text-xl capitalize border-2 border-b-black border-white hover:border-black py-1 px-4 hover:border-2 cursor-pointer"
+                key={index}
+                onClick={(e) => scrollToSection(e, item.id)}
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <nav className="fixed sm:hidden block top-0 bg-white w-full h-16 drop-shadow-md">
+          <h1 className="absolute left-10 top-5 font-bold text-xl">
+            Impact Summit
+          </h1>
+          {/* <h1 className="absolute right-10 top-5">⁝</h1> */}
+          {showNav && (
+            <ul className="flex items-center justify-center gap-10">
+              {menuItems.map((item, index) => (
+                <li
+                  className="text-xl capitalize border-2 border-b-black border-white hover:border-black py-1 px-4 hover:border-2 cursor-pointer"
+                  key={index}
+                  onClick={(e) => scrollToSection(e, item.id)}
+                >
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </nav>
+        <div className="flex min-h-screen flex-col items-center justify-start p-8 sm:p-20">
+          <div className="max-w-7xl w-full items-center justify-center">
+            <div className="w-full flex flex-col gap-2">
+              <h1
+                className={` text-3xl sm:text-6xl mb-5 font-bold text-center`}
+              >
+                <span>Impact Summit</span>
+                <br />
+                <span className="text-green-500">Nigeria</span> 2023.
+              </h1>
+              <div className="w-full aspect-video object-cover h-60 sm:h-[40rem]">
+                <Image
+                  src="/mountsummit.jpeg"
+                  alt="impact summit 2023"
+                  width={1000}
+                  height={1000}
+                  className="rounded-2xl object-cover w-full h-full"
+                />
+              </div>
+              <p
+                className={` text-left text-xl sm:text-2xl sm:mt-5 sm:text-center`}
+              >
+                Highlighting the impactful activities and organizations in the
+                region
+              </p>
+              <div className="w-full flex flex-col sm:flex-row gap-3 sm:gap-10 sm:items-center sm:justify-center">
+                <span
+                  className={` text-left text-xl sm:text-2xl flex items-center`}
+                >
+                  <div className="border border-black rounded-md bg-slate-100 mr-2">
+                    <Image
+                      src="/locationicon.svg"
+                      alt="impact summit 2023 venue is Sylvia Hotel, Enugu."
+                      width={10}
+                      height={10}
+                      className="rounded-2xl object-cover w-8 h-8"
+                    />
+                  </div>{" "}
+                  <p>Sylvia Hotel, Enugu.</p>
+                </span>
+                <span
+                  className={` text-left text-xl sm:text-2xl flex items-center`}
+                >
+                  <div className="border border-black rounded-md bg-slate-100 mr-2">
+                    <Image
+                      src="/calendaricon.svg"
+                      alt="impact summit 2023 calendar date is 9th Dec, 2023."
+                      width={10}
+                      height={10}
+                      className="rounded-2xl object-cover w-8 h-8"
+                    />
+                  </div>{" "}
+                  <p>9th Dec, 2023.</p>
+                </span>
+              </div>
+              <div className="flex w-full items-center justify-center gap-2 mt-2">
+                <Link
+                  href={"https://app.1w3.io/Links/ISN2023"}
+                  target={"_blank"}
+                  className="w-full sm:w-1/3 bg-black rounded-full sm:rounded-xl text-white h-12 font-bold text-xl text-center flex items-center justify-center"
+                >
+                  Get tickets
+                </Link>
+                <Link
+                  href={
+                    "mailto:greenpillnigeria@gmail.com?subject=Impact%20Summit%20Question&body=Hello%20"
+                  }
+                  className="w-full sm:w-1/3 bg-black rounded-full sm:rounded-xl text-white h-12 font-bold text-xl text-center flex items-center justify-center"
+                >
+                  Email us
+                </Link>
+              </div>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div
+              id="hackathon"
+              className="w-full flex flex-col items-center justify-center gap-2"
+            >
+              <h1 className={` text-3xl sm:text-4xl font-bold text-center`}>
+                Impact Hack
+              </h1>
+
+              <Image
+                src="/splash3.jpg"
+                alt=""
+                width={1000}
+                height={1000}
+                className="rounded-2xl"
+              />
+              <p
+                className={` text-left sm:text-center text-xl mt-5 sm:text-2xl`}
+              >
+                Are you ready to make a lasting impact in your community
+                leveraging Web3? We invite you to join the <b>Impact Hack</b>{" "}
+                hackathon, the ultimate hackathon event that will challenge your
+                skills and creativity.
+              </p>
+              <h1 className={` font-bold text-left`}>
+                1st Nov, 2023 - 1st Dec, 2023
+              </h1>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div className="w-full flex items-center justify-center">
+              <Link
+                href={"https://forms.gle/qiAAYRENsZ2Nibk8A"}
+                target={"_blank"}
+                className="w-full sm:w-1/3 bg-black rounded-full sm:rounded-xl text-white h-12 font-bold text-xl text-center flex items-center justify-center"
+              >
+                Register Now
+              </Link>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div className="w-full flex flex-col gap-2">
+              <h1 className={` text-xl sm:text-2xl font-bold`}>
+                Impact Hack Focus
+              </h1>
+              <p className={` text-xl sm:text-2xl text-left`}>
+                Develop a cutting-edge crypto payment application on the Public
+                Goods Network <b>(PGN)</b> that leverages the power of{" "}
+                <b>EIP-4337</b> to offer users a seamless and intuitive
+                experience for making USD payments on PGN.
+              </p>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div className="w-full flex flex-col gap-2">
+              <h1 className={` text-xl sm:text-2xl font-bold`}>
+                Impact Hack Details
+              </h1>
+              <ul className={` list-disc px-4 text-xl sm:text-2xl`}>
+                <li>Start Date: 1st November, 2023</li>
+                <li>Duration: One Month</li>
+                <li>Submission deadline: 1st December, 2023</li>
+                <li>Judging and Results: 9th December, 2023</li>
+              </ul>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div id="speakers" className="w-full flex flex-col gap-2">
+              <h1 className={` text-3xl sm:text-4xl font-bold text-center`}>
+                Impact Summit
+                <br />
+                speakers
+              </h1>
+              <ul className={` list-disc text-xl sm:text-2xl`}>
+                <li className="flex h-14 w-full mb-3 items-center justify-start gap-2">
+                  <div className="w-1/5 sm:w-1/12 h-full">
+                    <div className="w-14 h-14 rounded-full bg-slate-200">
+                      <Image
+                        src="/jimicohen.jpeg"
+                        alt="Jimi Cohen, speaker at impact summit"
+                        width={1000}
+                        height={1000}
+                        className="rounded-full object-cover w-full h-full"
+                      />
+                    </div>
+                  </div>
+                  <span className="w-5/6 text-lg">
+                    Jimi Cohen (Founder of Treegens DAO)
+                  </span>
+                </li>
+                <li className="flex h-14 w-full mb-3 items-center justify-start gap-2">
+                  <div className="w-1/5 sm:w-1/12 h-full">
+                    <div className="w-14 h-14 rounded-full bg-slate-200">
+                      <Image
+                        src="/annaelechukwu.jpeg"
+                        alt="Annaelechukwu, speaker at impact summit"
+                        width={1000}
+                        height={1000}
+                        className="rounded-full object-cover w-full h-full"
+                      />
+                    </div>
+                  </div>
+                  <span className="w-5/6 text-lg">
+                    Annaelechukwu (Cofounder of Blockchain UNN)
+                  </span>
+                </li>
+                <li className="flex h-14 w-full mb-3 items-center justify-start gap-2">
+                  <div className="w-1/5 sm:w-1/12 h-full">
+                    <div className="w-14 h-14 rounded-full bg-slate-200">
+                      <Image
+                        src="/unclesam.jpeg"
+                        alt="Uncle Sam, speaker at impact summit"
+                        width={1000}
+                        height={1000}
+                        className="rounded-full object-cover w-full h-full"
+                      />
+                    </div>
+                  </div>
+                  <span className="w-5/6 text-lg">
+                    Uncle Sam (Web3 influencer)
+                  </span>
+                </li>
+                <li className="flex h-14 w-full mb-3 items-center justify-start gap-2">
+                  <div className="w-1/5 sm:w-1/12 h-full">
+                    <div className="w-14 h-14 rounded-full bg-slate-200">
+                      <Image
+                        src="/christwinifeanyi.jpeg"
+                        alt="Christwin Christwin, speaker at impact summit"
+                        width={1000}
+                        height={1000}
+                        className="rounded-full object-cover w-full h-full"
+                      />
+                    </div>
+                  </div>
+                  <span className="w-5/6 text-lg">
+                    Christwin Christwin (Founder of SwitchElectric)
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div id="contact" className="w-full h-40 bg-black left-0 absolute">
+              <div className="w-full h-full text-white flex flex-col items-center justify-center">
+                <div className="flex gap-5 mb-4">
+                  <Link
+                    href={"https://t.me/+tzbcHRsTb802Yjc0"}
+                    className="bg-white rounded-full w-12 h-12 flex items-center justify-center"
+                  >
+                    <Image
+                      src="/telegram.png"
+                      alt="telegram"
+                      width={25}
+                      height={25}
+                    />
+                  </Link>
+                  <Link
+                    href={"https://chat.whatsapp.com/Bz25P7lc7la1WVdzq090Tj"}
+                    className="bg-white rounded-full w-12 h-12 flex items-center justify-center"
+                  >
+                    <Image
+                      src="/whatsapp.png"
+                      alt="whatsapp"
+                      width={25}
+                      height={25}
+                    />
+                  </Link>
+                </div>
+                <h1>Impact Summit</h1>
+                <h1>2023</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
