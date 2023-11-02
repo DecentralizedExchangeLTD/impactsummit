e486de51de5ded308ff4fce2fef948f0609fac1a
chukusuccess <successchuku5@gmail.com>
2023-11-02 20:54:14 +0100

    changed analytics gtag

--- app/layout.js
+++ app/layout.js
@@ -119,11 +119,11 @@ export default function RootLayout({ children }) {
       <Script
         strategy="afterInteractive"
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-E73X2YZ8HJ"
+        src="https://www.googletagmanager.com/gtag/js?id=G-TMST84VMJP"
       ></Script>
       <Script strategy="afterInteractive" id="google-analytics">
         {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); 
-        gtag('config', 'G-E73X2YZ8HJ');`}
+        gtag('config', 'G-TMST84VMJP');`}
       </Script>
       <body className={questrial.className}>{children}</body>
     </html>
